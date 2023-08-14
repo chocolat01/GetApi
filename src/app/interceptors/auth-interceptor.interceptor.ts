dd095e8e43c5e17c3083585ac10e1ee674f97604
chocolat01 <alphaamanda8@gmail.com>
2023-08-14 16:48:30 +0200

    add environments

--- src/app/interceptors/auth-interceptor.interceptor.ts
+++ src/app/interceptors/auth-interceptor.interceptor.ts
@@ -8,7 +8,9 @@ import { environment } from 'src/environments/environment.development';
 export class AuthInterceptorInterceptor implements HttpInterceptor {
   apiKey = environment.apiKey; // Utilisez la clé d'API de l'environnement
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
+  intercept(request: HttpRequest<unknown>, 
+    next: HttpHandler): 
+    Observable<HttpEvent<unknown>> {
     const modifiedRequest = request.clone({
       setHeaders: {
         'x-api-key': this.apiKey
